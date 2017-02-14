fd453a025bb31228f9f3a50ab38e698c46923ee4
msrgnt <sarg.lomb@gmail.com>
2017-02-14 14:52:47 -0800

    add hw_2 folder

---
+++ michael-msrgnt/hw_2/5-random-address-generator-bonus/app.js
@@ -0,0 +1,12 @@
+var numby = Math.ceil(Math.random() * 50000);
+var street = ["Poppy Street", "Elm Avenue", "dPoop View"];
+var city = ["Funk City", "Poop Town", "Los Angeles"];
+var state = ["CA", "NY", "MI", "XX"];
+var zippy = ["90210", "11111", "21242", "39402"];
+
+var randStr = Math.floor(Math.random() * street.length);
+var randyCi = Math.floor(Math.random() * city.length);
+var randySt = Math.floor(Math.random() * state.length);
+var randyZ = Math.floor(Math.random() * state.length);
+
+console.log(numby + " " + street[randStr] + ", " + city[randyCi] + " " + state[randySt] + ", " + zippy[randyZ]);
