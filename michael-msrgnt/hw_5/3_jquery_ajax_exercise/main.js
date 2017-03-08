6f6610badf1435a62e2c9f500c7ad599bf77dd6e
msrgnt <sarg.lomb@gmail.com>
2017-03-08 10:04:50 -0800

    added darksky url

--- michael-msrgnt/hw_5/3_jquery_ajax_exercise/main.js
+++ michael-msrgnt/hw_5/3_jquery_ajax_exercise/main.js
@@ -15,12 +15,17 @@ $("button").on("click", function(){
   var inny = JSON.stringify($("input").val());
   var innyB = $("input").val();
   var apiKey = "716699c512e51e0b079137a1a3d6a3a4";
+  var apiDark = "8ffef538ce32bef0af139971a8a4801c";
   var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + inny + "&APPID=" + apiKey;
   $.get(weatherUrl, function(response){
     var weather = (response.weather[0].description);
-    var temp = (response.main.temp);
+    var temp = response.main.temp;
+    var latDark = response.coord.lat;
+    var longDark = response.coord.lon;
     var tempC = Math.ceil((temp * 9/5) - 459.67);
-    console.log(response);
+    var darkUrl = "https://api.darksky.net/forecast/" + apiDark + "/" + latDark + "," + longDark;
+
+    console.log(darkUrl);
     var weatherS = JSON.stringify(weather);
       $("div").text("Currently, it's nothin' but " + weather + " in " + JSON.parse(inny));
       $(".divB").text(tempC + " degrees");
